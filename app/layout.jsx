dbf034c138c9102917df8ede137a749fd2c13ed8
manish <manishyadav09123@gmail.com>
2025-12-20 20:50:05 +0530

    added changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Property Pulse",
@@ -11,7 +13,9 @@ const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
